b73ca953de2a1de14659dee4416eb3553b2f49be
sahuashi <54184879+sahuashi@users.noreply.github.com>
2021-05-20 11:58:38 -0400

    Refactor popup

--- js/popup/popup.js
+++ js/popup/popup.js
@@ -1,5 +1,5 @@
-if(document.readyState === 'loading') {
-    document.addEventListener('DOMContentLoaded',restoreState);
+if (document.readyState === 'loading') {
+    document.addEventListener('DOMContentLoaded', restoreState);
 } else {
     restoreState;
 }
@@ -8,54 +8,54 @@ var reminders = document.getElementById("reminders");
 var power = document.getElementById("power");
 var submit = document.getElementById("submit");
 
-if(power){
+if (power) {
     addEventListener('click', disableEnable);
 }
 
-if(submit){
+if (submit) {
     addEventListener('submit', setAlarm);
 }
 
 function disableEnable() {
-    if(power.checked){
-        reminders.disabled=false;
-        if(!reminders.value){
+    if (power.checked) {
+        reminders.disabled = false;
+        if (!reminders.value) {
             reminders.value = 10;
         }
     }
-    else{
-        reminders.disabled=true;
+    else {
+        reminders.disabled = true;
     }
 }
 
-function setAlarm(e){
+function setAlarm(e) {
     e.preventDefault();
-    if(power.checked){
+    if (power.checked) {
         chrome.alarms.clearAll();
         let interval = parseInt(reminders.value);
-        chrome.alarms.create({periodInMinutes: interval});
-        chrome.browserAction.setBadgeText({text: 'ON'});
-        chrome.browserAction.setBadgeBackgroundColor({color: '#fbbf9e'});
+        chrome.alarms.create({ periodInMinutes: interval });
+        chrome.browserAction.setBadgeText({ text: 'ON' });
+        chrome.browserAction.setBadgeBackgroundColor({ color: '#fbbf9e' });
         saveState();
     }
-    else{
+    else {
         chrome.alarms.clearAll();
-        chrome.browserAction.setBadgeText({text: ''});
+        chrome.browserAction.setBadgeText({ text: '' });
         saveState();
     }
 }
 
-function restoreState(){
-    chrome.storage.local.get("P",function(res){
-        power.checked = res.P;
+function restoreState() {
+    chrome.storage.local.get("power", function (res) {
+        power.checked = res.power;
     });
-    chrome.storage.local.get("TI",function(res){
-        reminders.value = res.TI;
+    chrome.storage.local.get("time", function (res) {
+        reminders.value = res.time;
         disableEnable();
     });
 }
 
-function saveState(){
-    chrome.storage.local.set({"P": power.checked});
-    chrome.storage.local.set({"TI": reminders.value});
+function saveState() {
+    chrome.storage.local.set({ "power": power.checked });
+    chrome.storage.local.set({ "time": reminders.value });
 }
